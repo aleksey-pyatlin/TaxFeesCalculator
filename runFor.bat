6fec929afede6dfe4cded2ba41e375227f28459a
Aleksey Pyatlin <aleksey.pyatlin@digitalarch.com>
2015-01-13 14:32:29 +0300

    * Add clean console command between compile and report printing operations

--- runFor.bat
+++ runFor.bat
@@ -6,6 +6,7 @@ call %1
 rem build console application
 call build.bat
 
+cls 
 rem Run feesCalc with profile
 cd FeesCalculator.ConsoleApplication\bin\Debug\
 call FeesCalculator.ConsoleApplication.exe  -p "%profilePath%"
